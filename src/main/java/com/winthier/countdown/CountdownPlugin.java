a689c50f5994053590b55633c48349ad494505a6
StarTux <ths.loy@googlemail.com>
2020-08-01 21:14:59 +0200

    Use Sidebar plugin

--- src/main/java/com/winthier/countdown/CountdownPlugin.java
+++ src/main/java/com/winthier/countdown/CountdownPlugin.java
@@ -1,31 +1,23 @@
 package com.winthier.countdown;
 
+import com.cavetale.sidebar.PlayerSidebarEvent;
+import com.cavetale.sidebar.Priority;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.HashSet;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
-import java.util.UUID;
 import org.bukkit.ChatColor;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
-import org.bukkit.scoreboard.DisplaySlot;
-import org.bukkit.scoreboard.Objective;
-import org.bukkit.scoreboard.Scoreboard;
 
 public final class CountdownPlugin extends JavaPlugin implements Listener {
-    // Internal
-    Scoreboard scoreboard;
-    Objective sidebar;
     long startTime;
     long endTime;
     BukkitRunnable task;
-    Set<UUID> ignorers = new HashSet<UUID>();
     int seconds = 0;
     // Config
     boolean enabled;
@@ -37,7 +29,6 @@ public final class CountdownPlugin extends JavaPlugin implements Listener {
     public void onEnable() {
         saveDefaultConfig();
         getCommand("countdown").setExecutor(new CountdownCommand(this));
-        getCommand("sidebar").setExecutor(new SidebarCommand(this));
         start();
     }
 
@@ -46,35 +37,15 @@ public final class CountdownPlugin extends JavaPlugin implements Listener {
         stop();
     }
 
-    void setIgnore(Player player, boolean value) {
-        Scoreboard main = getServer().getScoreboardManager().getMainScoreboard();
-        if (value) {
-            ignorers.add(player.getUniqueId());
-            if (enabled && player.getScoreboard().equals(scoreboard)) {
-                player.setScoreboard(main);
-            }
-        } else {
-            ignorers.remove(player.getUniqueId());
-            if (enabled) {
-                if (player.getScoreboard().equals(main)) {
-                    player.setScoreboard(scoreboard);
-                }
-            }
-        }
-    }
-
     void start() {
         configure();
         if (enabled) {
-            setupScoreboard();
-            setupPlayers();
             getServer().getPluginManager().registerEvents(this, this);
         }
         startTask();
     }
 
     void stop() {
-        resetPlayers();
         stopTask();
     }
 
@@ -108,7 +79,6 @@ public final class CountdownPlugin extends JavaPlugin implements Listener {
             updateTimer();
             if (seconds++ > 10) {
                 seconds = 0;
-                setupPlayers();
             }
         } else {
             if (seconds++ > 60) {
@@ -141,55 +111,11 @@ public final class CountdownPlugin extends JavaPlugin implements Listener {
         for (int i = 0; i < messages.size(); ++i) messages.set(i, format(messages.get(i)));
     }
 
-    void setupScoreboard() {
-        scoreboard = getServer().getScoreboardManager().getNewScoreboard();
-        sidebar = scoreboard.registerNewObjective("countdown", "dummy");
-        sidebar.setDisplaySlot(DisplaySlot.SIDEBAR);
-        for (int i = 0; i < messages.size(); ++i) {
-            sidebar.getScore(messages.get(messages.size() - 1 - i)).setScore(i);
-        }
-        updateTimer();
-    }
-
     void updateTimer() {
         long now = System.currentTimeMillis();
         if (now >= endTime) {
             enabled = false;
-            resetPlayers();
             return;
-        }
-        long timeLeft = startTime - System.currentTimeMillis();
-        if (timeLeft < 0) {
-            timeLeft = 0;
-            String title = format("%s&cUnderway&r%s", titlePrefix, titleSuffix);
-            sidebar.setDisplayName(title);
-        } else {
-            long secs = timeLeft / 1000;
-            long minutes = secs / 60;
-            long hours = minutes / 60;
-            String title = format("%s&f%02d&3:&f%02d&3:&f%02d%s", titlePrefix,
-                                  hours, minutes % 60, secs % 60, titleSuffix);
-            sidebar.setDisplayName(title);
-        }
-    }
-
-    void setupPlayers() {
-        for (Player player : getServer().getOnlinePlayers()) {
-            if (!ignorers.contains(player.getUniqueId())) {
-                Scoreboard main = getServer().getScoreboardManager().getMainScoreboard();
-                if (player.getScoreboard().equals(main)) {
-                    player.setScoreboard(scoreboard);
-                }
-            }
-        }
-    }
-
-    void resetPlayers() {
-        for (Player player : getServer().getOnlinePlayers()) {
-            if (player.getScoreboard().equals(scoreboard)) {
-                Scoreboard main = getServer().getScoreboardManager().getMainScoreboard();
-                player.setScoreboard(main);
-            }
         }
     }
 
@@ -202,8 +128,26 @@ public final class CountdownPlugin extends JavaPlugin implements Listener {
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         if (!enabled) return;
-        if (!ignorers.contains(event.getPlayer().getUniqueId())) {
-            event.getPlayer().setScoreboard(scoreboard);
+    }
+
+    @EventHandler
+    public void onPlayerSidebar(PlayerSidebarEvent event) {
+        if (!enabled) return;
+        long timeLeft = startTime - System.currentTimeMillis();
+        List<String> lines = new ArrayList<>();
+        if (timeLeft < 0) {
+            timeLeft = 0;
+            String title = format("%s&cUnderway&r%s", titlePrefix, titleSuffix);
+            lines.add(title);
+        } else {
+            long secs = timeLeft / 1000;
+            long minutes = secs / 60;
+            long hours = minutes / 60;
+            String title = format("%s&f%02d&3:&f%02d&3:&f%02d%s", titlePrefix,
+                                  hours, minutes % 60, secs % 60, titleSuffix);
+            lines.add(title);
         }
+        lines.addAll(messages);
+        event.addLines(this, Priority.DEFAULT, lines);
     }
 }
